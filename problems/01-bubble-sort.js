7f36c7c57c10a8f990839ce7e9f54a7c2ce038ea
Antonio Davila-Olivares <ajd57@nau.edu>
2021-05-18 10:11:23 -0700

    completed problem 01

--- problems/01-bubble-sort.js
+++ problems/01-bubble-sort.js
@@ -4,11 +4,35 @@ function swap(array, idx1, idx2) {
   let temp = array[idx1];
   array[idx1] = array[idx2];
   array[idx2] = temp;
+  return array
 }
 
 function bubbleSort(array) {
+  debugger
   // your code here
+  let swapped = true;
+
+  while(swapped) {
+    swapped = false;
+    for (let i = 0; i < array.length; i++) {
+
+      let el = array[i];
+      let next = array[i+1];
+
+      // (el > next) ? swap(array, i, i + 1) && swapped === true : el === el && next === next;
+      if (el > next) {
+        array = swap(array, i , i+1)
+        swapped = true
+      }
+
+    }
+  }
+  return array
 }
+debugger
+let arr = [2, -1, 4, 3, 7, 3] // 
+bubbleSort(arr)
+
 
 module.exports = {
   bubbleSort,
